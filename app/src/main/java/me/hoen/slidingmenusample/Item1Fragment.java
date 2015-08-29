67e1202401987592cb4ca150bceeaef4ed7a2289
vhoen <vincent.hoen@gmail.com>
2015-08-29 16:29:29 +0100

    Initial commit

---
+++ app/src/main/java/me/hoen/slidingmenusample/Item1Fragment.java
@@ -0,0 +1,16 @@
+package me.hoen.slidingmenusample;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Item1Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup
+            container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_item_1, container, false);
+    }
+}
---
+++ app/src/main/java/me/hoen/slidingmenusample/MainActivity.java
@@ -0,0 +1,56 @@
+package me.hoen.slidingmenusample;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import java.util.ArrayList;
+
+import me.hoen.slidingmenu.BaseActivity;
+import me.hoen.slidingmenu.MenuItem;
+
+public class MainActivity extends BaseActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSlideMenu().setCloseAfterItemSelection(false);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        goHome();
+    }
+
+    @Override
+    protected void goHome() {
+        Fragment f = new HomeFragment();
+
+        getFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, f, "home").commit();
+        getFragmentManager().executePendingTransactions();
+    }
+
+    @Override
+    protected ArrayList<MenuItem> getSlidingMenuItems() {
+        final ArrayList<MenuItem> list = new ArrayList<>();
+
+        list.add(new MenuItem(getString(R.string.first_menu_item), R.mipmap.ic_launcher));
+
+        return list;
+    }
+
+    @Override
+    protected void onSideMenuClick(MenuItem item) {
+        if (item.getLabel().equals(getString(R.string.first_menu_item))) {
+            Fragment f = new Item1Fragment();
+
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame, f,
+                            "tag")
+                    .commit();
+            getFragmentManager().executePendingTransactions();
+        }
+    }
+}
